52a5a223ed69e0d2864dcd08a1bde9a0fc2d2790
eazynesta <nestaamwangi@gmail.com>
2024-07-08 17:14:38 +0300

    projects

--- lib/data.ts
+++ lib/data.ts
@@ -3,7 +3,7 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import FlappyNeural from "@/public/FlappyNeural.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
@@ -69,11 +69,11 @@ export const projectsData = [
     imageUrl: corpcommentImg,
   },
   {
-    title: "E-commerce website",
+    title: "Flappy Bird Using Deep Reinforcement Learning",
     description:
       "Developed a scalable and resilient e-commerce platform  and enhanced communication between fronted and backend system improving system performance and speeds.",
     tags: ["Python", "Django", "Next.js", "DRF", "SQLite", "Docker"],
-    imageUrl: rmtdevImg,
+    imageUrl: FlappyNeural,
   },
   {
     title: " Movie filter animation",
