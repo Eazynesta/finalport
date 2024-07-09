619a909c57fc90cb8d60f3a49911c4132fb5ae74
eazynesta <nestaamwangi@gmail.com>
2024-07-09 22:05:57 +0300

    thumbnails

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import FlappyNeural from "@/public/FlappyNeural.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import blockchain from "@/public/blockchain.jpg";
+import vector from "@/public/vector.jpg";
+import adblock from "@/public/adblock.jpg";
 
 export const links = [
   {
@@ -62,25 +62,25 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "Lizza-Chatbot",
+    title: "Custom Blockchain Implementation",
     description:
-      "Developed an interactive AI-powered pizza ordering chatbot using NLP, enhancing userengagements and providing real-time assistance.",
-    tags: ["Typescript", "Vue.js", "PostgreSQL", "DialogFlow"],
-    imageUrl: corpcommentImg,
+      "Developed a decentralized blockchain platform using JavaScript with an Angular-based user interface for secure and transparent transaction management.",
+    tags: ["Javascript", "Angular", "Express", "Node"],
+    imageUrl: blockchain,
   },
   {
-    title: "Flappy Bird Using Deep Reinforcement Learning",
+    title: "Vector Space Indexing Engine",
     description:
       "Developed a scalable and resilient e-commerce platform  and enhanced communication between fronted and backend system improving system performance and speeds.",
     tags: ["Python", "Django", "Next.js", "DRF", "SQLite", "Docker"],
-    imageUrl: FlappyNeural,
+    imageUrl: vector,
   },
   {
-    title: " Movie filter animation",
+    title: " Ad Blocking Extension",
     description:
       "Implemented an immersive movie filtering animation feature using React.",
     tags: ["React", "Next.js", "RESTful API", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: adblock,
   },
 ] as const;
 
